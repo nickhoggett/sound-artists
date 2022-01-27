f3edeffea202cfeb264ad15d2eed13b9634c1728
Nick Hoggett <nick.hoggett@gmail.com>
2022-01-27 18:52:54 +0000

    first commit

---
+++ src/componants/roster.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Roster() {
+    return (<div>
+        <p>WOOZE</p>
+        <p>Lennixx</p>
+        <p>Goat Girl</p>
+        <p>Rudi Salmon</p>
+        <p>whenyoung</p>
+        <p>Sound Artists logo</p>
+    </div>
+    )
+}
---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from "react";
+ 
+export default function FourOhFour() {
+    return <div>
+        <p>404 - this page doesn't exist please go back and try agin</p>
+    </div>
+}
